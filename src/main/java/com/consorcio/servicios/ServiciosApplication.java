e72030a4535cd7dee98b3f66b053d1bb288bb21f
LautiCabrera <lau.cabrera114@gmail.com>
2024-05-02 17:30:29 -0300

    Create proyect

---
+++ src/main/java/com/consorcio/servicios/ServiciosApplication.java
@@ -0,0 +1,13 @@
+package com.consorcio.servicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiciosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/consorcio/servicios/ServiciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consorcio.servicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
