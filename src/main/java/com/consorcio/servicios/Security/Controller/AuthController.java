36ee393b87bcb6b94b6dfed3321d848de0aa1408
Lautaro Cabrera <lau.cabrera114@gmail.com>
2024-05-21 21:11:10 -0300

    Change AuthResponse, add enum Role and delete JwtDto

--- src/main/java/com/consorcio/servicios/Security/Controller/AuthController.java
+++ src/main/java/com/consorcio/servicios/Security/Controller/AuthController.java
@@ -4,7 +4,6 @@ import com.consorcio.servicios.Security.Service.AuthService;
 import com.consorcio.servicios.Security.Entity.AuthResponse;
 import com.consorcio.servicios.Security.Entity.RegisterRequest;
 import com.consorcio.servicios.Security.Entity.LoginRequest;
-import com.consorcio.servicios.Security.Dto.JwtDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,8 +17,8 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping(value = "/login")
-    public ResponseEntity<JwtDto> login(@RequestBody LoginRequest request) {
-        JwtDto response = authService.login(request);
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+        AuthResponse response = authService.login(request);
         System.out.println(response);
         return ResponseEntity.ok(response);
     }
@@ -35,5 +34,5 @@ public class AuthController {
         AuthResponse response = authService.adminRegister(request);
         return response;
     }
-    
+
 }
--- src/main/java/com/consorcio/servicios/Security/Enums/Role.java
+++ src/main/java/com/consorcio/servicios/Security/Enums/Role.java
@@ -1,9 +1,10 @@
 package com.consorcio.servicios.Security.Enums;
 
 public enum Role {
-    
+
     ROLE_SUPERADMIN,
+    ROLE_ADMIN_USER,
     ROLE_ADMIN,
     ROLE_USER
-    
+
 }
