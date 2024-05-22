5a8e00b5fa6dcdc25f63d30956fa2661e05f702b
Lautaro Cabrera <lau.cabrera114@gmail.com>
2024-05-22 17:07:09 -0300

    Value mail.recover.expiration

--- src/main/java/com/consorcio/servicios/Security/Controller/AuthController.java
+++ src/main/java/com/consorcio/servicios/Security/Controller/AuthController.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,55 +20,57 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:5173")
 public class AuthController {
-    
+
     private final EmailService emailService;
     @Autowired
     private RecoverPassRepository recoverPassRepository;
     @Autowired
     private PasswordEncoder passwordEncoder;
-    
+    @Value("${mail.recover.expiration}")
+    long expiration;
+
     @PostMapping("/forgot-password")
     public ResponseEntity<?> forgotPassword(@RequestBody Map<String, String> request) {
         String email = request.get("email");
         Optional<User> userOptional = recoverPassRepository.findByUsername(email);
-        
+
         if (!userOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
         }
-        
+
         User user = userOptional.get();
         String token = UUID.randomUUID().toString();
         user.setResetToken(token);
-        user.setTokenExpiration(LocalDateTime.now().plusMinutes(15));
+        user.setTokenExpiration(LocalDateTime.now().plusMinutes(expiration));
         recoverPassRepository.save(user);
         emailService.sendResetPasswordEmail(email, token);
-        
+
         return ResponseEntity.ok("Correo electrónico de restablecimiento de contraseña enviado");
     }
-    
+
     @PostMapping("/reset-password")
     public ResponseEntity<?> resetPassword(@RequestBody Map<String, String> request) {
         String token = request.get("token");
         String newPassword = request.get("newPassword");
-        
+
         Optional<User> userOptional = recoverPassRepository.findByResetToken(token);
-        
+
         if (!userOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Token invalido");
         }
-        
+
         User user = userOptional.get();
-        
+
         if (user.getTokenExpiration().isBefore(LocalDateTime.now())) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Este link expiro");
         }
-        
+
         user.setPassword(passwordEncoder.encode(newPassword));
         user.setResetToken(null);
         user.setTokenExpiration(null);
         recoverPassRepository.save(user);
-        
+
         return ResponseEntity.ok("Contraseña modificada con exito");
     }
-    
+
 }
